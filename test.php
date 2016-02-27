cb5b8e53e9fe30a55c750eb9cf4163283344fe8a
Janre <janre.press@khk.ee>
2016-02-27 15:31:15 +0200

    Registration from project

--- test.php
+++ test.php
@@ -1,6 +1,34 @@
 <?php
 
-echo $_GET['name'];
+$con = mysqli_connect("localhost", "root", "", "test");
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
 
+$sel = "select * from users where email='$email'";
+
+$run = mysqli_query($con, $sel);
+
+$check_email = mysqli_num_rows($run);
+
+if ($check_email == 1) {
+
+
+    echo "<h2>This email is already registered, please try another!</h2>";
+    exit();
+
+
+} else {
+
+    $insert = "insert into users (name,email,pass) values ('$name','$email','$pass')";
+    $run_insert = mysqli_query($con, $insert);
+
+    if ($run_insert) {
+
+
+        echo "<h2>Registration successful, thanks!</h2>";
+
+    }
+}
 
 ?>
